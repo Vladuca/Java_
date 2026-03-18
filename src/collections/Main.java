ad352a2427c6976ce36e1ab47326a927fc970fca
Vladuca <bogdan_dima@ukr.net>
2026-03-18 19:15:23 +0200

    add_hvedya

--- src/collections/Main.java
+++ src/collections/Main.java
@@ -1,4 +1,4 @@
-package main;
+package collections;
 
 public class Main {
  public static void main(String[] args) {
@@ -7,7 +7,9 @@ public class Main {
 			 "Олег",
 			 "Валя",
 			 "Коля",
-			 "Петро"
+			 "Петро",
+			 "Василь",
+			 "Хведя",
 	 };
 	 
 	 String[] second = new String[employees.length + 1];
